6ca03d5fd149211769e73a6e84fd19a528d6afbc
Peng-Yu-2020 <Peng_Yu@student.uml.edu>
2022-09-14 10:46:53 -0400

    update useEffect and add some codes to catch error

--- src/Home.js
+++ src/Home.js
@@ -5,27 +5,36 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [ispending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
-        fetch("http://localhost:8000/blogs")
-        .then((result) => result.json())
-        .then(data => {
-            setBlogs(data)
-            setIsPending(false)
-        })
-        .catch(e => {
-            console.log(e.message);
-        })
+            fetch("http://localhost:8000/blogs")
+            .then((result) => {
+                if(!result.ok){
+                    throw Error("could not fetch the data from that resource!");}
+                return result.json();
+                })
+            .then(data => {
+                setError(null);
+                setBlogs(data);
+                setIsPending(false);
+            })
+            .catch(e => {
+                setIsPending(false);
+                setError(e.message)
+            })
         }, 1000)
     }, []);
 
     return (  
         <div className="home">
+        {error && <p>{ error }</p>}
         {ispending && <p>loading...</p>}
         {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
-}
+};
  
 export default Home;
+
