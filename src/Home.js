16bf4cb0dc9f9e0dde4902e3c870262f23eb25b4
Peng-Yu-2020 <Peng_Yu@student.uml.edu>
2022-09-14 06:28:39 -0400

    add ispending usestate

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,20 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
-
-    const [name, setName] = useState("peng");
-
+    const [ispending setIsPending] = useState(true);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
         .then((result) => result.json())
-        .then(data => setBlogs(data))
+        .then(data => {
+            setBlogs(data)
+            setIsPending(false)
+        })
     }, []);
 
     return (  
         <div className="home">
+        {ispending && <p>loading...</p>}
         {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
