b3fa5cfea04ddb20d75b66fd830eab35c409c8cb
Peng-Yu-2020 <Peng_Yu@student.uml.edu>
2022-09-13 21:34:21 -0400

    add useEffect in home.js

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,27 @@
-import {useState} from "react";
-import Blog from "./Blog";
+import {useState, useEffect} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'My old website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    function handleDelete(id){
+        setBlogs(
+            blogs.filter((blog) => blog.id !== id)
+        )
+    }
+
+    useEffect(() => {
+        console.log("use effect run");
+        console.log(blogs)
+    })
     return (  
         <div className="home">
-            {blogs.map((blog) => {
-               return  <Blog
-                key = {blog.id} 
-                title={blog.title} 
-                body={blog.body} 
-                author={blog.author}
-                id={blog.id}
-                 />})}
+           <BlogList handleDelete={handleDelete} blogs={blogs} title="All Blogs"/>
+           <BlogList handleDelete={handleDelete} blogs={blogs.filter((blog)=> blog.author === "mario")} title="Mario Blogs"/>
         </div>
     );
 }
